e39a196cc94e8057186830384e1cced4edcc2ae6
unknown <lucasrtinte19@gmail.com@>
2024-05-24 19:25:15 -0300

    logger entregable

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import express from "express";
-
+import { addLogger } from "./utils/logger.js";
 import productRoutes from "./routes/productRoutes.js";
 import mockingRoutes from "./routes/mockingRoutes.js";
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(addLogger);
 app.use("/products", productRoutes);
 app.use("/", mockingRoutes);
+
+app.get("/loggerTest", (req, res) => {
+  req.logger.debug("Este es un mensaje de debug");
+  req.logger.http("Este es un mensaje de http");
+  req.logger.info("Este es un mensaje de info");
+  req.logger.warning("Este es un mensaje de warning");
+  req.logger.error("Este es un mensaje de error");
+  req.logger.fatal("Este es un mensaje de fatal");
+  res.send(
+    "Prueba de logs completada. Revisa la consola y los archivos de logs."
+  );
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
